e0f196286d206138a6223a5afc9de438911e2f4c
NGUYEN QUANG LINH <nguyenquanglinh@topengnet.com>
2024-05-21 16:55:22 +0700

    FunctionView

--- FastechMotion/FastechMotion/ViewModels/MainViewModel.cs
+++ FastechMotion/FastechMotion/ViewModels/MainViewModel.cs
@@ -9,5 +9,8 @@ namespace FastechMotion.ViewModels
     public class MainViewModel : ViewModelBase
     {
         public ViewModelBase SingleMoveCurrentView { get; set; } = new SingleMoveViewModel();
+        public ViewModelBase JogMoveViewModel { get; set; } = new JogMoveViewModel();
+        public ViewModelBase OriginViewModel { get; set; } = new OriginViewModel();
+        public ViewModelBase FunctionViewModel { get; set; } = new FunctionViewModel();
     }
 }
